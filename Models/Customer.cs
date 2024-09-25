8369170ad341fff1d3f039dbb2cd717e39576218
Vigoliet <vigor.lundkvist@live.se>
2024-09-25 22:17:18 +0200

    Created base project with MYSQL connection

---
+++ Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string? CustomerName { get; set; }
+        public string? Address { get; set; }
+        public string? EmailAddress { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var connectionString = builder.Configuration.GetConnectionString("MySqlConn");
+
+            builder.Services.AddDbContext<AppDbContext>(options => {
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
